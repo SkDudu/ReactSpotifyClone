cd99b13c67385e5d99c87d07c2f8f7f68f110075
skdudu <dudusantosap@gmail.com>
2021-03-12 12:30:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+import logo from './spotify.svg';
+import fotoHome from './picture-home.jpg';
+import instagram from './instagram.svg'
+import facebook from './facebook.svg'
+import twitter from './twitter.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="nav-bar">
+        <img className="App-logo" src={logo} alt="Logo" />
+          <ul>
+            <a href="#">Premium</a>
+            <a href="#">Suporte</a>
+            <a href="#">Baixar</a>
+            <a>|</a>
+            <a href="#">Inscrever-se</a>
+            <a href="#">Entrar</a>
+          </ul>
+        </nav>
       </header>
+
+      <body>
+        <div>
+        <img className="Foto" src={fotoHome} alt="foto home"/>
+        <button className="BtnDownload">BAIXE O SPOTIFY</button>
+        <p>Milhões de musicas para ouvir. E nem precisa de cartão de crédito</p>
+        </div>
+      </body>
+
+      <footer>
+      <div class="wrapper">
+        <div class="logo">
+          <img className="App-logo" src={logo} alt="Logo" />
+        </div>
+        <div class="box b">
+          <ul className="nav-footer">
+            <li>
+              <a className="a-cinza">Empresa</a>
+            </li>
+            <li>
+              <a>Sobre</a>
+            </li>
+            <li>
+            <a>Empregos</a>
+            </li>
+            <li>
+            <a>For the record</a>
+            </li>
+          </ul>
+        </div>
+        <div class="box c">
+          <ul className="nav-footer">
+            <li>
+              <a className="a-cinza">Comunidade</a>
+            </li>
+            <li>
+              <a>Para artistas</a>
+            </li>
+            <li>
+            <a>Desenvolvedores</a>
+            </li>
+            <li>
+            <a>Publicidade</a>
+            </li>
+            <li>
+            <a>investidores</a>
+            </li>
+            <li>
+            <a>Fornecedores</a>
+            </li>
+          </ul>
+        </div>
+        <div class="box d">
+          <ul className="nav-footer">
+            <li>
+              <a className="a-cinza">Links Úteis</a>
+            </li>
+            <li>
+              <a>Suporte</a>
+            </li>
+            <li>
+              <a>Player na Web</a>
+            </li>
+            <li>
+              <a>Aplicativo móvel grátis</a>
+            </li>
+          </ul>
+        </div>
+        <div class="box e">
+          <img className="Social" src={instagram} alt="Logo" />
+          <img className="Social" src={twitter} alt="Logo" />
+          <img className="Social" src={facebook} alt="Logo" />
+        </div>
+      </div>
+        <div className="p-footer">
+          <a href="#">Legal</a>
+          <a href="#">Centro de Privacidade</a>
+          <a href="#">Política de privacidade</a>
+          <a href="#">Cookies</a>
+          <a href="#">Sobre anúncios</a>
+        </div>
+      </footer>
     </div>
   );
 }
